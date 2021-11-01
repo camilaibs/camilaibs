5a475bbe0c1fe79fa25acabe06455708cd44186c
Camila Belo <camilaibs@gmail.com>
2021-11-01 16:57:02 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Camila Belo
+
+I'm a _Software Engineer_, _Teacher_ and _Speaker_ that really love to work with JavaScript and popular libraries/frameworks such as React, Marko JS and AngularJS.
+Before it I worked as Backend developer, it was fundamental for me to understand how it works dealing with Monolithic applications, Microservices, Relational e Document based data bases, Async Processing and etc.
+I worked as well as a System Analyst, in this position I elaborated technical specifications to be used by Developers and validated their work.
+Additionally, I worked in IT Governance as a Process Analyst, which developed my ability to map business processes & identify deficiencies in order to build and apply solutions.
+
+📖 _Github:_ https://github.com/camilaibs
+
+🔗 _Stackoverflow:_ https://stackoverflow.com/cv/camila-belo
---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+console.log(`
+Camila Belo
+
+I'm a Software Engineer, Teacher and Speaker that really love to work with JavaScript and popular libraries/frameworks such as React, Marko JS and AngularJS.
+Before it I worked as Backend developer, it was fundamental for me to understand how it works dealing with Monolithic applications, Microservices, Relational e Document based data bases, Async Processing and etc.
+I worked as well as a System Analyst, in this position I elaborated technical specifications to be used by Developers and validated their work.
+Additionally, I worked in IT Governance as a Process Analyst, which developed my ability to map business processes & identify deficiencies in order to build and apply solutions.
+
+
+📖 Github:        https://github.com/camilaibs
+🔗 Stackoverflow: https://stackoverflow.com/cv/camila-belo
+`);
